54490df4ff50bdd12daf9bd7d9cf50330ec8ddb9
scripka <olga.morgan17@gmail.com>
2021-01-19 09:31:20 -0700

    Refactor apicalls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,4 +1,18 @@
-export const getOrders = () => {
-  return fetch('http://localhost:3001/api/v1/orders')
-      .then(response => response.json())
+const getData = (path) => {
+  return fetch(path).then((response) => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error(
+        "Sorry we are having difficulty loading this page, please try again later!"
+      );
+    }
+  });
+};
+
+export const apiCalls = {
+
+  getOrders: () => {
+    return getData(`http://localhost:3001/api/v1/orders`);
+  },
 }
