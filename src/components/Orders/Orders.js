12c642831df2fa73dbe5573820799884804b2ff5
scripka <olga.morgan17@gmail.com>
2021-01-19 10:39:34 -0700

    Refactor orders, add keys

--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -1,15 +1,14 @@
 import React from 'react';
 import './Orders.css';
 
-const Orders = props => {
-  const orderEls = props.orders.map(order => {
-    console.log(props.orders)
+const Orders = ({ orders }) => {
+  const orderEls = orders.map(order => {
     return (
-      <div className="order">
+      <div key={`${order.id}`} className="order">
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li key={`ing-${ingredient}`}>{ingredient}</li>
+            return <li key={`${order.id}-${ingredient}`}>{ingredient}</li>
           })}
         </ul>
       </div>
