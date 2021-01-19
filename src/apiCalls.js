a031a9619f9d8ecef9b46a828186f35d0a2a0817
scripka <olga.morgan17@gmail.com>
2021-01-19 09:50:17 -0700

    Add post request to api calls

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -10,9 +10,32 @@ const getData = (path) => {
   });
 };
 
+const updateData = (path, action, data) => {  
+  return fetch(path, {
+    method: action,
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  }).then((response) => {
+    if (response.ok) {
+      return response;
+    } else {
+      throw new Error(
+        "Sorry we are having difficulty loading this page, please try again later!"
+      );
+    }
+  });
+};
+
 export const apiCalls = {
 
   getOrders: () => {
     return getData(`http://localhost:3001/api/v1/orders`);
   },
+
+  postJobPosting: (data) => {
+    return updateData(`http://localhost:3001/api/v1/orders`, "POST", data);
+  }
 }
