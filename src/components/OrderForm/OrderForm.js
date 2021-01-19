6f092e0600834aa5ae863c3fdca04bfc93f96afc
scripka <olga.morgan17@gmail.com>
2021-01-19 09:51:05 -0700

    Add usestate to OrderForm, fix handleSubmit, add handleIngredientsChange

--- src/components/OrderForm/OrderForm.js
+++ src/components/OrderForm/OrderForm.js
@@ -1,30 +1,41 @@
-import React, { Component } from 'react';
-
-class OrderForm extends Component {
-  constructor(props) {
-    super();
-    this.props = props;
-    this.state = {
-      name: '',
-      ingredients: []
-    };
-  }
+import React, { useState, useEffect } from "react";
+import {apiCalls} from '../../apiCalls';
 
 
-  handleSubmit = e => {
+const OrderForm = () => {
+
+  const [name, setName] = useState("");
+  const [ingredients, setIngredients] = useState([]);
+  const [order, setOrder] = useState(null)
+
+  const handleSubmit = (e) => {
     e.preventDefault();
+    setOrder({
+      name: name,
+      ingredients: ingredients
+    })
     this.clearInputs();
   }
 
-  clearInputs = () => {
-    this.setState({name: '', ingredients: []});
+  const handleIngredientChange = (e) => {
+    e.preventDefault();
+    const isDuplicate = ingredients.find(ingredient => {
+      return e.target.name === ingredient;
+    })
+    if(!isDuplicate) {
+      setIngredients([...ingredients, e.target.name])
+    }
   }
 
-  render() {
+  const clearInputs = () => {
+    setName('');
+    setIngredients([])
+  }
+
     const possibleIngredients = ['beans', 'steak', 'carnitas', 'sofritas', 'lettuce', 'queso fresco', 'pico de gallo', 'hot sauce', 'guacamole', 'jalapenos', 'cilantro', 'sour cream'];
     const ingredientButtons = possibleIngredients.map(ingredient => {
       return (
-        <button key={ingredient} name={ingredient} onClick={e => this.handleIngredientChange(e)}>
+        <button key={ingredient} name={ingredient} onClick={e => handleIngredientChange(e)}>
           {ingredient}
         </button>
       )
@@ -36,20 +47,19 @@ class OrderForm extends Component {
           type='text'
           placeholder='Name'
           name='name'
-          value={this.state.name}
+          value={name}
           onChange={e => this.handleNameChange(e)}
         />
 
         { ingredientButtons }
 
-        <p>Order: { this.state.ingredients.join(', ') || 'Nothing selected' }</p>
+        <p>Order: { ingredients.join(', ') || 'Nothing selected' }</p>
 
-        <button onClick={e => this.handleSubmit(e)}>
+        <button onClick={e => handleSubmit(e)}>
           Submit Order
         </button>
       </form>
     )
-  }
 }
 
 export default OrderForm;
--- src/components/Orders/Orders.js
+++ src/components/Orders/Orders.js
@@ -9,7 +9,7 @@ const Orders = props => {
         <h3>{order.name}</h3>
         <ul className="ingredient-list">
           {order.ingredients.map(ingredient => {
-            return <li>{ingredient}</li>
+            return <li key={`ing-${ingredient}`}>{ingredient}</li>
           })}
         </ul>
       </div>
